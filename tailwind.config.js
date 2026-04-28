1384ea4bb96b9e08f9259abbd36f75bfb766c30e
arjshiv <arj.shiv@gmail.com>
2026-04-28 07:15:45 -0700

    Switch typography to Geist

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,8 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        display: ['"Azeret Mono"', 'monospace'],
-        mono: ['"Fragment Mono"', 'monospace'],
+        display: ['"Geist Mono"', 'monospace'],
+        mono: ['"Geist Mono"', 'monospace'],
       },
       colors: {
         text: {
